7758da2f0e0f57c7dc7d49183add321d30ddac01
Kevin Britt <kbritt510@gmail.com>
2020-08-09 00:58:36 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -15,7 +15,7 @@ class String
   end
 
   def count_sentences
-    self.split(/\.+|\?+|\!+/i).count do |sentence| 
+    self.split(/\.|\?+|\!+/i).count do |sentence| 
       !sentence = nil
     end
   end
